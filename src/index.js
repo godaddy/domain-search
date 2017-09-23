2feee873d47f6f7970c455efabbdf823223ba73d
Bryan Focht <bxfocht@godaddy.com>
2017-09-22 23:47:10 -0700

    change to snake case for compatibility with WordPress

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ import App from './App';
 const elements = document.getElementsByClassName('rstore-domain-search');
 Array.prototype.forEach.call(elements, element => {
   const text = {
-    placeholder: element.dataset.textPlaceholder || "Find your perfect domain name",
-    search: element.dataset.textSearch || "Search",
-    available: element.dataset.textAvailable || "Congrats, your domain is available!",
-    notAvailable: element.dataset.textNotAvailable || "Sorry that domain is taken",
-    cart: element.dataset.textCart || "Continue to Cart",
-    select: element.dataset.textSelect || "Select",
-    selected: element.dataset.textSelected || "Selected"
+    placeholder: element.dataset.text_placeholder || "Find your perfect domain name",
+    search: element.dataset.text_search || "Search",
+    available: element.dataset.text_available || "Congrats, your domain is available!",
+    notAvailable: element.dataset.text_not_available || "Sorry that domain is taken",
+    cart: element.dataset.text_cart || "Continue to Cart",
+    select: element.dataset.text_select || "Select",
+    selected: element.dataset.text_selected || "Selected"
   },
-  baseUrl = element.dataset.baseUrl||"secureserver.net",
-  pageSize = element.dataset.pageSize || "5";
+  baseUrl = element.dataset.base_url||"secureserver.net",
+  pageSize = element.dataset.page_size || "5";
 
   return ReactDOM.render(
     <App text={text} plid={element.dataset.plid} baseUrl={baseUrl} pageSize={pageSize} />,

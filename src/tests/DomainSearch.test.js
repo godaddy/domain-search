faad29db14b6bfbdb328eb2d721c41edddcf591a
Bryan Focht <bxfocht@godaddy.com>
2017-09-28 16:18:39 -0700

    add unit tests

--- src/tests/DomainSearch.test.js
+++ src/tests/DomainSearch.test.js
@@ -34,9 +34,41 @@ describe('DomainSearch', () => {
   it('should render spinner when searching', () => {
     const wrapper = shallow(<DomainSearch {...props} />);
 
-    wrapper.setState({ searching: true, completed: false });
+    wrapper.setState({ searching: true });
 
     expect(wrapper.find('.rstore-loading')).toHaveLength(1);
+  });
+
+  it('should render continue button when domain is available', () => {
+    const wrapper = shallow(<DomainSearch {...props} />);
+
+    wrapper.setState({ searching: false, addingToCart: false, exactDomain : { available: true }, suggestedDomains : []  });
+
+    expect(wrapper.find('.rstore-domain-continue-button')).toHaveLength(1);
+  });
+
+  it('should render spinner when adding to cart', () => {
+    const wrapper = shallow(<DomainSearch {...props} />);
+
+    wrapper.setState({ searching: false, addingToCart: true, exactDomain : { available: true }, suggestedDomains : []  });
+
+    expect(wrapper.find('.rstore-loading')).toHaveLength(1);
+  });
+
+  it('should render error when adding to cart errors', () => {
+    const wrapper = shallow(<DomainSearch {...props} />);
+
+    wrapper.setState({ searching: false, addingToCart: false, error: true, exactDomain : { available: true }, suggestedDomains : []  });
+
+    expect(wrapper.find('.rstore-error')).toHaveLength(1);
+  });
+
+  it('should render error when domain search errors', () => {
+    const wrapper = shallow(<DomainSearch {...props} />);
+
+    wrapper.setState({ searching: false, addingToCart: false, error: true });
+
+    expect(wrapper.find('.rstore-error')).toHaveLength(1);
   });
 
   it('should not call handleDomainSearch when an empty form is submitted', () => {
@@ -92,4 +124,57 @@ describe('DomainSearch', () => {
 
     expect(wrapper.find(<SearchResults {...searchProps} />));
   });
+
+  it('should add exact domain when continue to cart button is clicked', () => {
+    const wrapper = shallow(<DomainSearch {...props} />);
+
+    const spy = sandbox.spy(util, 'fetch');
+
+    wrapper.setState({ searching: false, addingToCart: false, exactDomain : { available: true, domain:'', listPrice: '0.00' }, suggestedDomains : []  });
+
+    wrapper.find('.rstore-domain-continue-button').simulate('click', { preventDefault() {} });
+
+    expect(spy.called).toBeTruthy();
+  });
+
+  it('should add selected domains when continue to cart button is clicked', () => {
+    const wrapper = shallow(<DomainSearch {...props} />);
+
+    const spy = sandbox.stub(util, 'fetch').callsFake(() => Promise.resolve({
+       cartUrl: '#'
+    }));
+
+    wrapper.setState({ selectedDomains:[ 'asdf.com'], searching: false, addingToCart: false, exactDomain : { available: true, domain:'', listPrice: '0.00' }, suggestedDomains : []  });
+
+    wrapper.find('.rstore-domain-continue-button').simulate('click', { preventDefault() {} });
+
+    expect(spy.called).toBeTruthy();
+  });
+
+  it('should set error in state when continue to cart button is clicked and api has an error', () => {
+    const wrapper = shallow(<DomainSearch {...props} />);
+
+    const spy = sandbox.stub(util, 'fetch').callsFake(() => Promise.resolve({
+       error: 'domain no longer available'
+    }));
+
+    wrapper.setState({ selectedDomains:[ 'asdf.com'], searching: false, addingToCart: false, exactDomain : { available: true, domain:'', listPrice: '0.00' }, suggestedDomains : []  });
+
+    wrapper.find('.rstore-domain-continue-button').simulate('click', { preventDefault() {} });
+
+    expect(spy.called).toBeTruthy();
+  });
+
+  it('should do nothing when continue to cart button is clicked and there is no cartUrl and no error', () => {
+    const wrapper = shallow(<DomainSearch {...props} />);
+
+    const spy = sandbox.stub(util, 'fetch').callsFake(() => Promise.resolve({}));
+
+    wrapper.setState({ selectedDomains:[ 'asdf.com'], searching: false, addingToCart: false, exactDomain : { available: true, domain:'', listPrice: '0.00' }, suggestedDomains : []  });
+
+    wrapper.find('.rstore-domain-continue-button').simulate('click', { preventDefault() {} });
+
+    expect(spy.called).toBeTruthy();
+  });
+
 });
